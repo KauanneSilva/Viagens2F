fcb2dd00897f79fb93ef0aa669a1388bd44f61c2
Kauanne <126569333+KauanneSilva@users.noreply.github.com>
2023-10-29 11:43:21 -0300

    Update Validacao.java
    validar cpf

--- workspace/Viagens2F/src/Validacao.java
+++ workspace/Viagens2F/src/Validacao.java
@@ -1,6 +1,60 @@
-public class Validacao {
-    public static String cpf(String s) {
+import java.util.Scanner;
 
-    return String.valueOf(true);
-   }
+public class Validacao {
+    public static void main(String[] args) {
+        System.out.println("Digite seu cpf (somente números): ");
+        Scanner tcd = new Scanner(System.in);
+        String cpf = tcd.nextLine();
+
+        if (Util.validaCPF(cpf)) {
+            System.out.println("CPF OK");
+        } else {
+            System.out.println("CPF Inválido!");
+        }
+
+        tcd.close();
+    }
+}
+
+class Util {
+    public static boolean validaCPF(String cpf) {
+        if (cpf == null || cpf.length() != 11 || !cpf.chars().allMatch(Character::isDigit)) {
+            return false;
+        }
+
+        char firstDigit = cpf.charAt(0);
+        boolean allDigitsAreSame = true;
+        for (int i = 1; i < 11; i++) {
+            if (cpf.charAt(i) != firstDigit) {
+                allDigitsAreSame = false;
+                break;
+            }
+        }
+        if (allDigitsAreSame) {
+            return false;
+        }
+
+        int[] numbers = new int[11];
+        for (int i = 0; i < 11; i++) {
+            numbers[i] = Character.getNumericValue(cpf.charAt(i));
+        }
+
+        for (int i = 0; i < 2; i++) {
+            int sum = 0;
+            for (int j = 0; j < 9 + i; j++) {
+                sum += numbers[j] * (10 + i - j);
+            }
+
+            int checkDigit = sum % 11;
+            checkDigit = checkDigit < 2 ? 0 : 11 - checkDigit;
+
+            if (checkDigit != numbers[9 + i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 }
